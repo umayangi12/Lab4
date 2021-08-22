f6bcd8e15ad794980762d5cbbb9ddd2b814c2960
Umayangi12 <umayangi1999@gmail.com>
2021-08-22 21:37:12 +0530

    Commit002

---
+++ app/src/main/java/com/example/lab4/MyServices.java
@@ -0,0 +1,36 @@
+package com.example.lab4;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MyServices  extends Service {
+    private MediaPlayer player;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player = MediaPlayer.create(this,
+                Settings.System.DEFAULT_NOTIFICATION_URI);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+
+
+}
